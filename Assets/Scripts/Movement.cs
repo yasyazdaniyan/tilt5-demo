e6f592b50a072e6070a8b1dec7535858e743fdb0
Temp name due to official comp <hej@svt.se>
2023-09-25 18:20:18 +0200

    first commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TiltFive;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,27 +8,43 @@ public class Movement : MonoBehaviour
 {
     public GameObject gameObject1;
     public float speed = 1.0f;
+    public float zoomSpeed = 1.0f;
+    float zoomAmount = 1.0f;
+    
     public void MoveCube(InputAction.CallbackContext context) {
         this.transform.Translate(context.ReadValue<Vector2>().x * Time.deltaTime * speed, 0, context.ReadValue<Vector2>().y * Time.deltaTime * speed);
     }
 
-    public void RotateCube(InputAction.CallbackContext context) {
-        this.transform.Rotate(0,context.ReadValue<Vector2>().x * Time.deltaTime * speed,0);
-    }
+    
     void Update(){
         if(TiltFive.Wand.TryGetWandDevice(TiltFive.PlayerIndex.One,TiltFive.ControllerIndex.Right, out TiltFive.WandDevice wandDevice))
         {
-            Debug.Log("Hi");
-            if (wandDevice.One.isPressed)
+            
+            if (wandDevice.X.isPressed)
             {
-                Debug.Log("Slay detected");
+                
                 gameObject1.transform.Rotate(0,Time.deltaTime * speed,0);
             }
-            if (wandDevice.Two.isPressed)
+            if (wandDevice.B.isPressed)
             {
-                Debug.Log("Slay detected");
+                
                 gameObject1.transform.Rotate(0,-Time.deltaTime * speed,0);
+                
+                
             }
-        }
+            if(wandDevice.One.isPressed){
+                zoomAmount += zoomSpeed * Time.deltaTime;
+                TiltFiveManager2.Instance.playerOneSettings.gameboardSettings.currentGameBoard.localScale = zoomAmount;
+            }
+             if(wandDevice.Two.isPressed){
+                zoomAmount -= zoomSpeed * Time.deltaTime;
+                if(zoomAmount <= 0) zoomAmount = 0.01f;
+                TiltFiveManager2.Instance.playerOneSettings.gameboardSettings.currentGameBoard.localScale = zoomAmount;
+            }
+   
+        
     }
 }
+}
+ 
+
